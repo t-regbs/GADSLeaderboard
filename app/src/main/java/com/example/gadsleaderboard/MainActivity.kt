45c45ef55b09c572f24dccf1838cfec2cbe11c30
Timilehin <aregbestimi@gmail.com>
2020-09-03 00:25:50 +0100

    setup tablayout and viewpager

--- app/src/main/java/com/example/gadsleaderboard/MainActivity.kt
+++ app/src/main/java/com/example/gadsleaderboard/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.gadsleaderboard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gadsleaderboard.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        // removing toolbar elevation
+        binding.topAppBar.elevation = 0f
+
+        binding.viewPager.adapter = ViewPagerFragmentAdapter(this)
+
+        // attaching tab mediator
+        TabLayoutMediator(
+            binding.tabLayout, binding.viewPager
+        ) { tab: TabLayout.Tab, position: Int -> tab.text = titles[position] }.attach()
     }
 }
---
+++ app/src/main/java/com/example/gadsleaderboard/ViewPagerFragmentAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.gadsleaderboard
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+val titles = arrayOf("Learning Leaders", "Skill IQ Leaders")
+class ViewPagerFragmentAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> return LearningLeadersFragment()
+            1 -> return IqLeadersFragment()
+        }
+        return LearningLeadersFragment()
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+}
