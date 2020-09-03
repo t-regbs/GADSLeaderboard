7bbab9e3c1c4c8c3ade9b1890d0a28814afac463
Timilehin <aregbestimi@gmail.com>
2020-09-03 03:59:59 +0100

    add observers

--- app/src/main/java/com/example/gadsleaderboard/ui/fragments/IqLeadersFragment.kt
+++ app/src/main/java/com/example/gadsleaderboard/ui/fragments/IqLeadersFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.example.gadsleaderboard.IqLeaderAdapter
 import com.example.gadsleaderboard.data.api.ApiHelper
 import com.example.gadsleaderboard.data.api.RetrofitBuilder
 import com.example.gadsleaderboard.databinding.FragmentIqLeadersBinding
+import com.example.gadsleaderboard.utils.Status
 
 class IqLeadersFragment : Fragment() {
 
     private lateinit var binding: FragmentIqLeadersBinding
-    private var leadersViewModel: IqLeadersViewModel? = null
+    private lateinit var leadersViewModel: IqLeadersViewModel
+    private val adapter = IqLeaderAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +31,38 @@ class IqLeadersFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentIqLeadersBinding.inflate(inflater, container, false)
+        binding = FragmentIqLeadersBinding.inflate(inflater, container, false).apply {
+            iqLeadersRv.adapter = adapter
+        }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        leadersViewModel.getIqLeaders().observe(viewLifecycleOwner, {
+            it?.let { resource ->
+                when (resource.status) {
+                    Status.SUCCESS -> {
+                        binding.iqLeadersRv.visibility = View.VISIBLE
+//                        progressBar.visibility = View.GONE
+                        resource.data?.let { leaders -> adapter.submitList(leaders) }
+                    }
+                    Status.ERROR -> {
+                        binding.iqLeadersRv.visibility = View.VISIBLE
+//                        progressBar.visibility = View.GONE
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                    }
+                    Status.LOADING -> {
+//                        progressBar.visibility = View.VISIBLE
+                        binding.iqLeadersRv.visibility = View.GONE
+                    }
+                }
+            }
+        })
     }
 
 }
--- app/src/main/java/com/example/gadsleaderboard/ui/fragments/LearningLeadersFragment.kt
+++ app/src/main/java/com/example/gadsleaderboard/ui/fragments/LearningLeadersFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import com.example.gadsleaderboard.LearningLeaderAdapter
 import com.example.gadsleaderboard.data.api.ApiHelper
 import com.example.gadsleaderboard.data.api.RetrofitBuilder
 import com.example.gadsleaderboard.databinding.FragmentLearningLeadersBinding
+import com.example.gadsleaderboard.utils.Status
 
 class LearningLeadersFragment : Fragment() {
 
     private lateinit var binding: FragmentLearningLeadersBinding
-    private var leadersViewModel: LearningLeadersViewModel? = null
+    private lateinit var leadersViewModel: LearningLeadersViewModel
+    private val adapter = LearningLeaderAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +32,39 @@ class LearningLeadersFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLearningLeadersBinding.inflate(inflater, container, false)
+        binding = FragmentLearningLeadersBinding.inflate(inflater, container, false).apply {
+            learningLeadersRv.adapter = adapter
+        }
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        leadersViewModel.getLearningLeaders().observe(viewLifecycleOwner, {
+            it?.let { resource ->
+                when (resource.status) {
+                    Status.SUCCESS -> {
+                        binding.learningLeadersRv.visibility = View.VISIBLE
+//                        progressBar.visibility = View.GONE
+                        resource.data?.let { leaders -> adapter.submitList(leaders) }
+                    }
+                    Status.ERROR -> {
+                        binding.learningLeadersRv.visibility = View.VISIBLE
+//                        progressBar.visibility = View.GONE
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                    }
+                    Status.LOADING -> {
+//                        progressBar.visibility = View.VISIBLE
+                        binding.learningLeadersRv.visibility = View.GONE
+                    }
+                }
+            }
+        })
+    }
+
 
 }
