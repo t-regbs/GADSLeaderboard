d8f30f0a6d840477e415a9e74d6d83c36dc05b1c
Timilehin <aregbestimi@gmail.com>
2020-09-04 11:34:25 +0100

    updated ui

--- app/src/main/java/com/example/gadsleaderboard/ui/FormActivity.kt
+++ app/src/main/java/com/example/gadsleaderboard/ui/FormActivity.kt
@@ -1,10 +1,11 @@
 package com.example.gadsleaderboard.ui
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.gadsleaderboard.R
 import com.example.gadsleaderboard.databinding.ActivityFormBinding
+import com.example.gadsleaderboard.databinding.DialogResultEnteredBinding
 
 class FormActivity : AppCompatActivity() {
 
@@ -15,7 +16,27 @@ class FormActivity : AppCompatActivity() {
         binding = ActivityFormBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.topAppBar)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        setToolbar()
+    }
+
+    private fun setToolbar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        binding.topAppBar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun createResponseDialog(res: Int, text: String) {
+        val dialog = layoutInflater.inflate(R.layout.dialog_result_entered, null)
+        val binding = DialogResultEnteredBinding.bind(dialog)
+
+        val builder: AlertDialog = AlertDialog.Builder(this).create()
+        builder.setView(dialog)
+        builder.show()
+
+        binding.submissionResponse.text = text
+        binding.img.setImageResource(res)
     }
 
 }
